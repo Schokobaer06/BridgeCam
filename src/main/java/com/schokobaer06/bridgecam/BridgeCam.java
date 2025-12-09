8fdc7d3ebaf5ee628073bf11e245c1cdfc994c6b
Josef Peer <josef.schokobaer@gmail.com>
2025-12-09 10:52:24 +0100

    setup build.gradle & mods.toml

---
+++ src/main/java/com/schokobaer06/bridgecam/BridgeCam.java
@@ -0,0 +1,12 @@
+package com.schokobaer06.bridgecam;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BridgeCam.MOD_ID)
+public class BridgeCam {
+    public static final String MOD_ID = "bridgecam";
+
+    public BridgeCam() {
+
+    }
+}
