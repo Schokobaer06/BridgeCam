f42e6a8702f9ea93bcdb629a16f03f756d97934b
Josef Peer <josef.schokobaer@gmail.com>
2025-12-10 15:59:23 +0100

    added creative tab

--- src/main/java/com/schokobaer06/bridgecam/BridgeCam.java
+++ src/main/java/com/schokobaer06/bridgecam/BridgeCam.java
@@ -1,15 +1,11 @@
 package com.schokobaer06.bridgecam;
 
 import com.schokobaer06.bridgecam.init.InitBlocks;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
+import com.schokobaer06.bridgecam.init.InitTabs;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod(BridgeCam.MOD_ID)
 public class BridgeCam {
@@ -19,15 +15,6 @@ public class BridgeCam {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
         InitBlocks.register(bus);
-
-        bus.addListener(this::addCreative);
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        for (RegistryObject<Item> item: InitBlocks.ITEMS.getEntries()) {
-            if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
-                event.accept(item.get());
-            }
-        }
+        InitTabs.CREATIVE_MODE_TABS.register(bus);
     }
 }
---
+++ src/main/java/com/schokobaer06/bridgecam/init/InitTabs.java
@@ -0,0 +1,23 @@
+package com.schokobaer06.bridgecam.init;
+
+import com.schokobaer06.bridgecam.BridgeCam;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class InitTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BridgeCam.MOD_ID);
+    public static final RegistryObject<CreativeModeTab> CREATIVE_TAB = CREATIVE_MODE_TABS.register("creative_mode_tab",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.bridgecam.tab"))
+                    .icon(() -> new ItemStack(InitBlocks.MONITOR_BLOCK.get()))
+                    .displayItems((parameters, tabData) -> {
+                        InitBlocks.ITEMS.getEntries().forEach(
+                                itemRegistryObject -> tabData.accept(itemRegistryObject.get())
+                        );
+                    })
+                    .build());
+}
